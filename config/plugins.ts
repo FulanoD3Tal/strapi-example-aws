66a86099468bd5233f384fedb61b57e8a515a0bb
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-08-22 10:23:24 -0600

    feat:add s3 upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,4 +5,19 @@ export default ({ env }) => ({
       // See `Config` section.
     },
   },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
+        },
+      },
+    },
+  },
 });
