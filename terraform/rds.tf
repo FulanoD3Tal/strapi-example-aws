1788b763b4d0670db1ad96f38bab8610b5605680
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-09-08 09:15:20 -0600

    feat: connect rds to ec2 instance only

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,3 +1,27 @@
+resource "aws_security_group" "strapi_rds_sg" {
+  name        = "strapi-rds-sg"
+  description = "strapi rds server security group"
+  vpc_id      = aws_vpc.strapi_vpc.id
+  tags = {
+    Name = "strapi"
+  }
+  ingress {
+    description     = "rds port ingress"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.strapi_server_sg.id]
+  }
+}
+
+resource "aws_db_subnet_group" "strapi_rds_subnet_group" {
+  name        = "strapi-rds-subnet-group"
+  description = "RDS subnet group for tutorial"
+
+  subnet_ids = [aws_subnet.private_subnet_1, aws_subnet.private_subnet_2]
+}
+
+
 resource "aws_db_instance" "strapi_db" {
   identifier        = "strapi"
   instance_class    = "db.t3.micro"
@@ -8,8 +32,8 @@ resource "aws_db_instance" "strapi_db" {
   db_name           = var.db_name
   password          = var.db_password
 
-  db_subnet_group_name   = aws_db_subnet_group.strapi_db_snet_group.name
-  vpc_security_group_ids = [aws_security_group.strapi-rds-group.id]
+  db_subnet_group_name   = aws_db_subnet_group.strapi_rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.strapi_rds_sg.id]
   publicly_accessible    = true
   skip_final_snapshot    = true
   tags = {
